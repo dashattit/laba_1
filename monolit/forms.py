2804fdbb56de879e1f6231d5bee7c84c85cde1ef
dashattit <dashyla.2021@mail.ru>
2024-11-28 15:03:32 +0700

    register

---
+++ monolit/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+from .models import AddUser
+from django.core.validators import RegexValidator, EmailValidator
+
+
+class AddUserCreatingForm(forms.ModelForm):
+    username = forms.CharField(
+        label="Имя пользователя",
+        max_length=100,
+        widget=forms.TextInput()
+    )
+    email = forms.CharField(
+        label="Почта",
+        max_length=100,
+        widget=forms.EmailInput(),
+        validators=[
+            EmailValidator(message="Почта должна содержать обязательный символ @"),
+        ]
+    )
+    first_name = forms.CharField(
+        label="Имя",
+        max_length=100,
+        widget=forms.TextInput(),
+        validators=[
+            RegexValidator(
+                message="Имя должно содержать только кириллические буквы, дефис и пробелы",
+            )
+        ]
+    )
+    last_name = forms.CharField(
+        label="Фамилия",
+        max_length=100,
+        widget=forms.TextInput(),
+        validators=[
+            RegexValidator(
+                message="Фамилия должна содержать только кириллические буквы, дефис и пробелы",
+            )
+        ]
+    )
+    patronym = forms.CharField(
+        label="Отчество",
+        max_length=100,
+        widget=forms.TextInput(),
+        validators=[
+            RegexValidator(
+                message="Отчество должно содержать только кириллические буквы, дефис и пробелы",
+            )
+        ]
+    )
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            self.add_error('password_confirm', "Пароли не совпадают")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = AddUser
+        fields = ("username", "email", "first_name", "last_name", "patronym", "password", "password_confirm")
+
+
+class AddUserLoginForm(forms.Form):
+    username = forms.CharField(
+        label="Логин",
+        max_length=100,
+        widget=forms.TextInput()
+    )
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
---
+++ monolit/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import UserProfileListView
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', UserProfileListView.as_view(), name='profile'),
+]
